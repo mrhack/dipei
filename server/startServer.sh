485912643fcce4a2f587b9d1098148555b0fbe94
13leaf <sd6733531@gmail.com>
2013-06-20 23:43:03 +0800

    setup server

--- server/startServer.sh
+++ server/startServer.sh
@@ -1,6 +1,6 @@
 sudo mkdir -p /data/logs/nginx
 sudo pkill -f nginx php-fpm
 sudo nginx -c `pwd`/lepei.conf &
-sudo php-fpm &
+sudo php-fpm -c etc --fpm-config etc/php-fpm.conf&
 sudo nginx -V
 sudo mongod&

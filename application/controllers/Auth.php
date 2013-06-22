bec52491ad23e61339114323c04f49cf0fdbb1eb
13leaf <sd6733531@gmail.com>
2013-06-22 20:41:26 +0800

    setup server

---
+++ application/controllers/Auth.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * User: wangfeng
+ * Date: 13-6-22
+ * Time: 下午5:00
+ */
+class AuthController extends  BaseController
+{
+    public function authAction()
+    {
+
+    }
+}
---
+++ tests/models/TestDataFlow.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * User: wangfeng
+ * Date: 13-6-22
+ * Time: 下午7:24
+ */
+require_once __DIR__ . '/../DipeiTestCase.php';
+
+class TestDataFlow extends DipeiTestCase
+{
+
+}

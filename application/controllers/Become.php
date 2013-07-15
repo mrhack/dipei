0ddc13baf44f1ff25895aa783f4ce7f17408a958
mrhack <hdg1988@gmail.com>
2013-07-15 23:57:29 +0800

    fix

---
+++ application/controllers/Become.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * User: wangfeng
+ * Date: 13-6-30
+ * Time: 下午9:09
+ */
+class BecomeController extends BaseController
+{
+    public function validateAuth()
+    {
+        return true;
+    }
+
+    public function indexAction($uid)
+    {
+
+    }
+}

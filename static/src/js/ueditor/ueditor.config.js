5ba3ab911d932228592dcc45482c454da05712a0
mrhack <hdg1988@gmail.com>
2013-07-19 01:11:31 +0800

    fix

--- static/src/js/ueditor/ueditor.config.js
+++ static/src/js/ueditor/ueditor.config.js
@@ -135,7 +135,7 @@
 
         //图片上传配置区
         ,imageUrl: "/image/uploadForEditor/"             //图片上传提交地址
-        ,imagePath: "/public/img/"
+        ,imagePath: ""
         //,imagePath:URL + "php/"                     //图片修正地址，引用了fixedImagePath,如有特殊需求，可自行配置
         //,imageFieldName:"upfile"                   //图片数据的key,若此处修改，需要在后台对应文件修改对应参数
         //,compressSide:0                            //等比压缩的基准，确定maxImageSideLength参数的参照对象。0为按照最长边，1为按照宽度，2为按照高度

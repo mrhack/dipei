fa65077ae224637c69338a3f41a7d92ae85ce3fd
13leaf <sd6733531@gmail.com>
2013-09-12 11:47:23 +0800

    refactor tests

---
+++ runTest.sh
@@ -0,0 +1,16 @@
+#run test
+binPath=$(pear config-get bin_dir)
+phpunit=$binPath/phpunit
+
+for testdir in $(ls -d tests/**/) ; do
+   cd $testdir
+   for test in $(ls *Test.php) ; do
+      echo $test
+      if ! $phpunit -v $test ; then
+         echo "Test Failure!"
+         read
+         exit
+      fi
+   done
+   cd -
+done

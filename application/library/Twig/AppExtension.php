244426578b7691d36acb5fb2d5d7aec5a6df9e82
mrhack <hdg1988@gmail.com>
2013-09-30 11:52:18 +0800

    fis

--- application/library/Twig/AppExtension.php
+++ application/library/Twig/AppExtension.php
@@ -68,7 +68,14 @@ class Twig_AppExtension extends Twig_Extension{
                 preg_match("/_(\d+)-(\d+)(_(\d+)-(\d+))?\.\w+$/" , $url , $match );
                 if( $match ){
                     if( $match[3] ){
-                        return $match[2] / $match[1] * $match[4];
+                        if( $match[4] == 0 ){ // for: xxxxx/xx_111-222_0-40.png
+                            return $match[5];
+                        } else if( $match[5] == 0 ){ // for: xxxxx/xx_111-222_50-0.png
+                            return $match[2] / $match[1] * $match[4];
+                        } else { // for: xxxxx/xx_111-222_50-40.png
+                            return $match[2] / $match[1] > $match[4] / $match[5] ? 
+                                
+                        }
                     }
                     return $match[2];
                 }

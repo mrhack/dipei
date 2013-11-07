10373d9d51bfe977721c55fa8808ba20cb811fcf
mrhack <hdg1988@gmail.com>
2013-11-07 12:58:12 +0800

    fix

--- static/src/js/pagejs/search.js
+++ static/src/js/pagejs/search.js
@@ -1,4 +1,4 @@
-LP.use(['jquery' , 'util'/*,'datepicker'*/] , function( $ , util ){
+LP.use(['jquery' , 'util'] , function( $ , util ){
     /*
     $(".j-datepicker" ).datepicker({
         showOn: 'click',
@@ -10,28 +10,33 @@ LP.use(['jquery' , 'util'/*,'datepicker'*/] , function( $ , util ){
     });
 
     */
+    util.searchLoc($('.search-body input[type="text"]') , function( data ){
+        var type = $('.search-body input[name="type"]').val();
+        window.location.href = '/loc/' + data.id + '/?type=' + type;
+    }, 'city');
+    
+    
+    // // show lepei type
+    // $('.J_dropdown .dropdown-menu').on('click' , 'li a' , function(){
+    //     $(this)
+    //         .closest('.J_dropdown')
+    //         .find('.input-val')
+    //         .html( $(this).text() )
+    //         .end()
+    //         .find('input[type="hidden"]')
+    //         .val( $(this).data('value') );
+    // });
 
-    // show lepei type
-    $('.J_dropdown .dropdown-menu').on('click' , 'li a' , function(){
-        $(this)
-            .closest('.J_dropdown')
-            .find('.input-val')
-            .html( $(this).text() )
-            .end()
-            .find('input[type="hidden"]')
-            .val( $(this).data('value') );
-    });
-
-    $('.search-body')
-        .submit(function(){
-            var data = LP.query2json( $(this).serialize() );
-            if( !data.lid ){
-                util.error($('input[name="lid"]').closest('.input-widget'));
-                return false;
-            }
-            window.location.href = '/loc/' + data.lid + '/?type=' + (data.type || '');
-            return false;
-        });
+    // $('.search-body')
+    //     .submit(function(){
+    //         var data = LP.query2json( $(this).serialize() );
+    //         if( !data.lid ){
+    //             util.error($('input[name="lid"]').closest('.input-widget'));
+    //             return false;
+    //         }
+    //         window.location.href = '/loc/' + data.lid + '/?type=' + (data.type || '');
+    //         return false;
+    //     });
 
 
 });
